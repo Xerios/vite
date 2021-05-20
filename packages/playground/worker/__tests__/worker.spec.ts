b1aa87cfc3f1d2b184fe403642a46c22dbaf42a7
Sam <Xerios@users.noreply.github.com>
2021-05-20 18:39:38 +0200

    Update worker.spec.ts

--- packages/playground/worker/__tests__/worker.spec.ts
+++ packages/playground/worker/__tests__/worker.spec.ts
@@ -24,6 +24,6 @@ if (isBuild) {
     // chunk
     expect(content).toMatch(`new Worker("/assets`)
     // inlined
-    expect(content).toMatch(`new Worker("data:application/javascript`)
+    expect(content).toMatch(`new Worker(objURL`)
   })
 }
